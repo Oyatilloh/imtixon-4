b01ae67406a6cf14c442d2a4c92a650019a32a97
Oyatilloh <77911448+Oyatilloh@users.noreply.github.com>
2021-04-12 00:10:51 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+// const users = [
+// 		{id:1,name:'Sherzod',contact:'998901643551'},
+// 		{id:2,name:'Anvar',contact:'998901643551'},
+// 		{id:3,name:'Alisher',contact:'998901643551'},
+// 		{id:4,name:'Komil',contact:'998901643551'},
+// 		{id:5,name:'Ravshan',contact:'998901643551'},
+// 	]
+
+
+
+
+// let contactlist =document.querySelector('.users-information')
+// let contactItem =document.querySelector('#contact-item')
+
+//  function usersRenderer(array) {
+//  	for(let element of array) {
+//  		let li = document.createElement('li')
+//  		let h3 = document.createElement('h3')
+//  		let span = document.createElement('span')
+
+//  		li.classList.add("contact-item")
+//  		h3.textContent = element.name
+//  		span.textContent = element.contact
+
+//  		li.appendChild(span)
+//  		li.appendChild(h3)
+//  		contactlist.appendChild(li)
+//  	}
+//  }
+// usersRenderer(users)
+ 
+  
+
+
